b39ed411e14f8720f07a7f4084331d79b3e7def1
elliotgaramendi <elliotgaramendi@gmail.com>
2024-01-07 21:13:34 -0500

    chore: Configure images

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,12 @@
-import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
+import { defineConfig } from 'astro/config';
 
 import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), react()]
+  integrations: [tailwind(), react()],
+  image: {
+    remotePatterns: [{ protocol: "https" }],
+  }
 });

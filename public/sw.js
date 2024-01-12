cd4bb086dd568090aa74325a99eda4c174ac045d
elliotgaramendi <elliotgaramendi@gmail.com>
2024-01-12 11:43:12 -0500

    chore: Improve PWA installation

---
+++ public/sw.js
@@ -0,0 +1,18 @@
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open('my-cache').then((cache) => {
+      return cache.addAll([
+        '/',
+        '/manifest.json'
+      ]);
+    })
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
